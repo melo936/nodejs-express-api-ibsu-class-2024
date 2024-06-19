bfbe9979b4e366e1e4434d90a04b5958b1579022
anri-1151 <anri.morchiladze@gmail.com>
2024-06-19 12:55:21 +0400

    add-authentication

--- routes/students.js
+++ routes/students.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const studentService = require('../services/studentService');
+const ApiSecurity = require('../middleware/apiSecurity');
 
-router.get('/all', studentService.getAll);
-router.get('/:id', studentService.getOne);
-router.post('/add', studentService.add);
-router.delete('/:id', studentService.delete);
-router.put('/:id', studentService.update);
+router.get('/all', ApiSecurity.requireLogin, studentService.getAll);
+router.get('/:id', ApiSecurity.requireLogin, studentService.getOne);
+router.post('/add', ApiSecurity.requirePermits('manage_student'), studentService.add);
+router.delete('/:id', ApiSecurity.requirePermits('manage_student'), studentService.delete);
+router.put('/:id', ApiSecurity.requirePermits('manage_student'), studentService.update);
 
 module.exports = router;
